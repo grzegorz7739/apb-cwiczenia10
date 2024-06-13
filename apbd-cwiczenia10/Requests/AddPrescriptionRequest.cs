eb40b5384cc33479534e069d7ce0ba08d2cfc045
grzegorz7739 <162711272+grzegorz7739@users.noreply.github.com>
2024-06-13 23:00:20 +0200

    Update AddPrescriptionRequest.cs

--- apbd-cwiczenia10/Requests/AddPrescriptionRequest.cs
+++ apbd-cwiczenia10/Requests/AddPrescriptionRequest.cs
@@ -3,9 +3,9 @@ namespace apbd_cwiczenia10.Requests;
 public class AddPrescriptionRequest
 {
     public NewPatientRequest Patient { get; set; }
-    public DateTime Date { get; set; } // Data wystawienia recepty
-    public DateTime DueDate { get; set; } // Termin realizacji recepty
-    public int DoctorId { get; set; } // Id lekarza
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+    public int DoctorId { get; set; }
 
-    public List<PrescriptionMedicamentRequest> Medicaments { get; set; } // Lista leków na recepcie
+    public List<PrescriptionMedicamentRequest> Medicaments { get; set; }
 }
